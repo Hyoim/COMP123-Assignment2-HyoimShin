8ca5714efcccef39bc82835519dea2219079cdff
Hyoim SHin <hi0217@gmail.com>
2015-02-05 14:24:52 -0500

    Added SuperHero class

---
+++ COMP123-Assignment2-HyoimShin/Hero.cs
@@ -0,0 +1,124 @@
+﻿/***********************************************
+ * Hyoim Shin (300802252)
+ * 30 January, 2015
+ * Assignment1 : Implement Hero Class
+ * Revision History : 
+ * - Changed return type of hitDamage 
+ * - Added comment and program header
+ * ********************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment2_HyoimShin
+{
+    class Hero
+    {
+        Random rnd = new Random();   // is used to generate random number
+
+        // PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++++++++++
+        public string name;
+
+        // PRIVATE PROPERTIES ++++++++++++++++++++++++++++++++++++++++
+        protected int strength = 0;
+        protected int speed = 0;
+        protected int health = 0;
+
+        // CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++
+        public Hero(string name)
+        {
+            this.name = name;
+            generateAblities(rnd);
+        }
+
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++++
+
+        //calls the hitAttempt method that determines sucess or failure of attack
+        public void fight()
+        {
+            bool hitResult;                  // success or failure for attack
+            int damage, damageFact;          // is used to calculate damage
+
+            hitResult = hitAttempt(rnd);     
+
+            if (hitResult == true)
+            {
+                damageFact = hitDamage(rnd);    
+
+                // damage = hero's strength * damageFact
+                damage = this.strength * damageFact;
+
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Attack succeeded");
+                Console.WriteLine("The target is attacked by {0}", damage);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Attack failed");
+                Console.WriteLine();
+            }
+        }
+
+        //displays hero's ability score
+        public void show()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine(String.Format("The ability of {0}", this.name).PadLeft(42 - (14 - (this.name.Length / 2))));
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+
+            Console.WriteLine("{0, 22} {1, -20}", "Strength:", this.strength);
+            Console.WriteLine("{0, 22} {1, -20}", "Speed:", this.speed);
+            Console.WriteLine("{0, 22} {1, -20}", "Health:", this.health);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+        }
+
+
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++++
+
+        // generates the ability numbers for the strength, speed, health 
+        private void generateAblities(Random rnd)
+        {
+            // Random class is used for making random hero's ability between 1 to 100 
+            this.strength = rnd.Next(1, 101);
+            this.speed = rnd.Next(1, 101);
+            this.health = rnd.Next(1, 101);
+        }
+
+        //determines sucess or failure of hero's attack 
+        private bool hitAttempt(Random rnd)
+        {
+            // Random class is used for determining success or failure for attack
+            // Hero hits would succeed 20% of the time
+
+            int hitAttempt = rnd.Next(1, 11);
+            Console.Write("{0} is attempting attack", this.name);
+            Console.WriteLine();
+
+            if ((hitAttempt == 1) || (hitAttempt == 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //calculates the damage the Hero causes to the target on a hit
+        private int hitDamage(Random rnd)
+        {
+            // Random class is used for generating hit factor
+            // the factor between 1 and 6 is used to calculate the hit damage
+
+            int hitDamageFact = rnd.Next(1, 7);
+            return hitDamageFact;
+        }
+    }
+}
---
+++ COMP123-Assignment2-HyoimShin/SuperHero.cs
@@ -0,0 +1,94 @@
+﻿/********************************************************
+ * Hyoim Shin (300802252)
+ * February 5, 2015
+ * Assignment2 : Implement SuperHero class by using Hero class
+ * Revision History : 
+ * - Crated constructor of SuperHero by using Hero Class
+ * - Added generateRandomPowers method
+ * ******************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment2_HyoimShin
+{
+    class SuperHero : Hero
+    {
+        Random rnd = new Random();
+
+        // PRIVATE PROPERTIES +++++++++++++++++++++++++++++++++++++++++
+        private string[] superPowerLists = { "Super Speed", "Super Strength", "Body Armour", "Flight", "Fire Generation", "Weather Control" };
+        private bool[] checkDuplicateArr = new bool[6];    // is used to check whethere the value of superPowerList is already used or not(false->not use) 
+        private string[] superPower = new string[3];
+
+        private int randomNum;
+
+        // CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++
+        public SuperHero(string name)
+            : base(name)
+        {
+            generateRandomPowers();
+        }
+
+        // PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++++++
+        public void showPowers()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine(String.Format("Super power of {0}", this.name).PadLeft(42 - (14 - (this.name.Length / 2))));
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+
+            for (int index = 0; index < this.superPower.Length; index++)
+            {
+                Console.WriteLine(String.Format("{0}", this.superPower[index]).PadLeft(42 - (21 - (this.superPower[index].Length / 2))));
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+
+        }
+
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++++
+        // Generates three random superpowers
+        private void generateRandomPowers()
+        {
+            initialCheckArray(checkDuplicateArr);
+
+            int count = 0;
+            while (count < superPower.Length)
+            {
+                randomNum = rnd.Next(6);
+
+                // If the random number is not used, input the value of random number in superPowerLists to superPower array
+                if (checkDuplicateArr[randomNum] == false)
+                {
+                    this.superPower[count] = superPowerLists[randomNum];
+                    checkDuplicateArr[randomNum] = true;
+                }
+                // If the random number is already used, it generates other random number
+                else if (checkDuplicateArr[randomNum] == true)
+                {
+                    while (checkDuplicateArr[randomNum] != false)
+                    {
+                        randomNum = rnd.Next(6);
+                    }
+                    this.superPower[count] = superPowerLists[randomNum];
+                    checkDuplicateArr[randomNum] = true;
+                }
+                count++;
+            }
+        }
+
+        // Initializes the value of checkValueArray as false
+        private void initialCheckArray(bool[] checkValueArray)
+        {
+            int index = 0;
+            while (index < checkValueArray.Length)
+            {
+                this.checkDuplicateArr[index] = false;
+                index++;
+            }
+        }
+    }
+}
